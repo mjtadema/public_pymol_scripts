d5c4188444dcbe672db74113fb03823ff6ada7a1
Matthijs Tadema <M.J.Tadema@protonmail.com>
2020-12-13 22:15:02 +0100

    Added some code to avoid the rate limiter. Caching json responses for 2 hours should do the trick.

--- loader.py
+++ loader.py
@@ -7,15 +7,40 @@ import requests
 from pymol import cmd
 import sys
 from pathlib import Path
+from datetime import datetime
+import json
 
-# Query the git repo for all files in root of branch master 
-response = requests.get(r'https://api.github.com/repos/mjtadema/public_pymol_scripts/contents?ref=master')
-if response.status_code is not 200:
-    raise Exception(response.text)
-objects = response.json()
+class ResponseOutOfDate(Exception): pass
+class RateLimit(Exception): pass
+
+cachedir = Path.home()
+response_file = cachedir/"pymol_loader.json"
+try:
+    # Check for a cached repo response
+    if not response_file.exists():
+        raise FileNotFoundError
+    previous_response_time = datetime.fromtimestamp(response_file.stat().st_mtime)
+    delta = datetime.now() - previous_response_time
+    delta_hours = delta.seconds / 3600
+    # Check if file is out of date
+    if delta_hours >= 2:
+        raise ResponseOutOfDate
+    print("Reading cached list")
+    with response_file.open() as rfh:
+        objects = json.load(rfh)
+except (FileNotFoundError, ResponseOutOfDate):
+    # Query the git repo for all files in root of branch master 
+    print("Querying for new list")
+    response = requests.get(r'https://api.github.com/repos/mjtadema/public_pymol_scripts/contents?ref=master')
+    if response.status_code != 200:
+        raise RateLimit("Hit rate limiter, try again later")
+    # Cache the response to file to avoid rate limiter
+    with open(response_file, 'w') as cache_out:
+        cache_out.write(response.text) 
+    objects = response.json()
 
 # Search for pymol modules
-scriptname = Path(sys.argv[0]).name
+scriptname = "loader.py"
 modules = {}
 for obj in objects:
     name = obj['name']
