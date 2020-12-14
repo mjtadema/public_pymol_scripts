f7f512ca20c38728e0669791fd2c3391ccdaafec
Matthijs Tadema <M.J.Tadema@protonmail.com>
2020-12-14 11:44:04 +0100

    Loader now caches scripts for the same time as the response code

--- loader.py
+++ loader.py
@@ -10,33 +10,46 @@ from pathlib import Path
 from datetime import datetime
 import json
 
+
 class ResponseOutOfDate(Exception): pass
 class RateLimit(Exception): pass
 
-cachedir = Path.home()
+
+def delta_hours(somepath: Path):
+    "return the hours delta to now"
+    previous_response_time = datetime.fromtimestamp(somepath.stat().st_mtime)
+    delta = datetime.now() - previous_response_time
+    return delta.seconds / 3600
+
+
+cachedir = Path.home()/"mjtadema_pymol_cache"
+cachedir.mkdir(exist_ok=True)
 response_file = cachedir/"pymol_loader.json"
+
 try:
     # Check for a cached repo response
     if not response_file.exists():
         raise FileNotFoundError
-    previous_response_time = datetime.fromtimestamp(response_file.stat().st_mtime)
-    delta = datetime.now() - previous_response_time
-    delta_hours = delta.seconds / 3600
+
     # Check if file is out of date
-    if delta_hours >= 2:
+    if delta_hours(response_file) >= 2:
         raise ResponseOutOfDate
+
     print("Reading cached list")
     with response_file.open() as rfh:
         objects = json.load(rfh)
+
 except (FileNotFoundError, ResponseOutOfDate):
     # Query the git repo for all files in root of branch master 
     print("Querying for new list")
     response = requests.get(r'https://api.github.com/repos/mjtadema/public_pymol_scripts/contents?ref=master')
     if response.status_code != 200:
         raise RateLimit("Hit rate limiter, try again later")
+
     # Cache the response to file to avoid rate limiter
     with open(response_file, 'w') as cache_out:
         cache_out.write(response.text) 
+
     objects = response.json()
 
 # Search for pymol modules
@@ -48,7 +61,18 @@ for obj in objects:
     if name.endswith(".py"):
         modules[obj['name']] = obj['download_url']
 
-# Load all the modules and inform the user
+# Update cached modules
 for module, url in modules.items():
-    cmd.run(url)
+    try:
+        module_path = Path(cachedir/module)
+        assert module_path.exists()
+        assert delta_hours(module_path) <= 2
+    except AssertionError:
+        print(f"Caching {module}")
+        with open(module_path, 'wb') as fout:
+            reponse = requests.get(url)
+            fout.write(reponse.content)
+    # Finally load the module and inform the user
+    cmd.run(str(module_path.absolute()))
     print(f"Loaded {module}")
+    
